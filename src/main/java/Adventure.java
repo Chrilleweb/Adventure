580ab196ee2b328d77d8a0cbaa06df0e3c18725b
Chrilleweb <96743881+Chrilleweb@users.noreply.github.com>
2022-09-27 09:15:04 +0200

    first commit

---
+++ src/main/java/Adventure.java
@@ -0,0 +1,47 @@
+public class Adventure {
+
+    Room room1 = new Room("Room1", "Your are now in room 1....");
+    Room room2 = new Room("Room2", "Your are now in room 2....");
+    Room room3 = new Room("Room3", "Your are now in room 3....");
+    Room room4 = new Room("Room4", "Your are now in room 4....");
+    Room room5 = new Room("Room5", "Your are now in room 5....");
+    Room room6 = new Room("Room6", "Your are now in room 6....");
+    Room room7 = new Room("Room7", "Your are now in room 7....");
+    Room room8 = new Room("Room8", "Your are now in room 8....");
+    Room room9 = new Room("Room9", "Your are now in room 8....");
+
+
+    public String getRoom1(){
+        return room1.getRoomName();
+    }
+    public String getRoom2(){
+        return room2.getRoomName();
+    }
+    public String getRoom3(){
+        return room3.getRoomName();
+    }
+    public String getRoom4(){
+        return room4.getRoomName();
+    }
+    public String getRoom5(){
+        return room5.getRoomName();
+    }
+    public String getRoom6(){
+        return room6.getRoomName();
+    }
+    public String getRoom7(){
+        return room7.getRoomName();
+    }
+    public String getRoom8(){
+        return room8.getRoomName();
+    }
+    public String getRoom9(){
+        return room9.getRoomName();
+    }
+
+    /*public String getEast(){
+        room1 = room2;
+    }*/
+
+}
+
---
+++ src/main/java/Userinterface.java
@@ -0,0 +1,193 @@
+import java.util.Scanner;
+
+public class Userinterface {
+    Scanner scan = new Scanner(System.in);
+    Adventure adventure = new Adventure();
+    String userInput;
+    String currentPlace = adventure.getRoom1();
+
+    public void user(){
+        do {
+            System.out.println("You are now in " + currentPlace +   " Where do you wanna go?");
+            userInput = scan.nextLine();
+            switch (userInput) {
+                case "go north":
+                    System.out.println("Going north");
+                    if (currentPlace == adventure.getRoom1()){
+                        System.out.println("You cant go north from " + currentPlace);
+                    }
+
+                    else if (currentPlace == adventure.getRoom2()) {
+                        System.out.println("you cant go north from " + currentPlace);
+                    }
+
+                    else if (currentPlace == adventure.getRoom3()) {
+                        System.out.println("You cant go north from " + currentPlace);
+                    }
+
+                    else if (currentPlace == adventure.getRoom4()) {
+                        currentPlace = adventure.getRoom1();
+                    }
+
+                    else if (currentPlace == adventure.getRoom5()) {
+                        System.out.println("You cant go north from " + currentPlace);
+                    }
+
+                    else if (currentPlace == adventure.getRoom6()) {
+                        currentPlace = adventure.getRoom3();
+                    }
+
+                    else if (currentPlace == adventure.getRoom7()) {
+                        currentPlace = adventure.getRoom4();
+                    }
+
+                    else if (currentPlace == adventure.getRoom8()) {
+                        currentPlace = adventure.getRoom5();
+                    }
+
+                    else if (currentPlace == adventure.getRoom9()) {
+                        currentPlace = adventure.getRoom6();
+                    }
+                    break;
+
+                case "go south":
+                    System.out.println("Going south");
+                    if (currentPlace == adventure.getRoom1()){
+                        currentPlace = adventure.getRoom4();
+                    }
+
+                    else if (currentPlace == adventure.getRoom2()) {
+                        System.out.println("Door is locked, go another way");
+                    }
+
+                    else if (currentPlace == adventure.getRoom3()) {
+                        currentPlace = adventure.getRoom6();
+                    }
+
+                    else if (currentPlace == adventure.getRoom4()) {
+                        currentPlace = adventure.getRoom7();
+                    }
+
+                    else if (currentPlace == adventure.getRoom5()) {
+                        currentPlace = adventure.getRoom8();
+                    }
+
+                    else if (currentPlace == adventure.getRoom6()) {
+                        currentPlace = adventure.getRoom9();
+                    }
+
+                    else if (currentPlace == adventure.getRoom7()) {
+                        System.out.println("You cant go south from " + currentPlace);
+                    }
+
+                    else if (currentPlace == adventure.getRoom8()) {
+                        System.out.println("You cant go south from " + currentPlace);
+                    }
+
+                    else if (currentPlace == adventure.getRoom9()) {
+                        System.out.println("You cant go south from " + currentPlace);
+                    }
+
+                    break;
+
+                case "go east":
+                    System.out.println("Going east");
+                    if (currentPlace == adventure.getRoom1()){
+                        currentPlace = adventure.getRoom2();
+                    }
+
+                    else if (currentPlace == adventure.getRoom2()) {
+                        currentPlace = adventure.getRoom3();
+                    }
+
+                    else if (currentPlace == adventure.getRoom3()) {
+                        System.out.println("You cant go east from " + currentPlace);
+                    }
+
+                    else if (currentPlace == adventure.getRoom4()) {
+                        System.out.println("You cant go east from " + currentPlace);
+                    }
+
+                    else if (currentPlace == adventure.getRoom5()) {
+                        System.out.println("You cant go east from " + currentPlace);
+                    }
+
+                    else if (currentPlace == adventure.getRoom6()) {
+                        System.out.println("You cant go east from " + currentPlace);
+                    }
+
+                    else if (currentPlace == adventure.getRoom7()) {
+                        currentPlace = adventure.getRoom8();
+                    }
+
+                    else if (currentPlace == adventure.getRoom8()) {
+                        currentPlace = adventure.getRoom9();
+                    }
+
+                    else if (currentPlace == adventure.getRoom9()) {
+                        System.out.println("You cant go east from " + currentPlace);
+                    }
+                    break;
+
+                case "go west":
+                    System.out.println("Going west");
+                    if (currentPlace == adventure.getRoom1()){
+                        System.out.println("You cant go west from " + currentPlace);
+                    }
+
+                    else if (currentPlace == adventure.getRoom2()) {
+                        currentPlace = adventure.getRoom1();
+                    }
+
+                    else if (currentPlace == adventure.getRoom3()) {
+                        currentPlace = adventure.getRoom2();
+                    }
+
+                    else if (currentPlace == adventure.getRoom4()) {
+                        System.out.println("You cant go west from " + currentPlace);
+                    }
+
+                    else if (currentPlace == adventure.getRoom5()) {
+                        System.out.println("You cant go west from " + currentPlace);
+                    }
+
+                    else if (currentPlace == adventure.getRoom6()) {
+                        System.out.println("You cant go west from " + currentPlace);
+                    }
+
+                    else if (currentPlace == adventure.getRoom7()) {
+                        System.out.println("You cant go west from " + currentPlace);
+                    }
+
+                    else if (currentPlace == adventure.getRoom8()) {
+                        currentPlace = adventure.getRoom7();
+                    }
+
+                    else if (currentPlace == adventure.getRoom9()) {
+                        currentPlace = adventure.getRoom8();
+                    }
+                    break;
+
+                case "look":
+                    System.out.println("look around");
+                    break;
+
+                case "help":
+                    System.out.println("print info");
+                    break;
+
+                case "exit":
+                    System.exit(0);
+                    break;
+            }
+        } while(userInput != "exit");
+
+
+
+
+    }
+
+    public void start(){
+        user();
+    }
+}
